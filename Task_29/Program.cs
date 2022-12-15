93e89b6bc99a783ff0b8724a31e15b40309e53c2
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-15 14:18:36 +0700

    Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 10);
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
