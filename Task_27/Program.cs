f5a080a76ce6916174004848944c36613565c764
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-15 14:13:51 +0700

    Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine()), n=a, c=0;
+while (n > 0)
+    {
+    c = c + n % 10;
+    n = n / 10;
+    }
+Console.WriteLine($"Сумма цифр чиcла {a} равна {c}");
