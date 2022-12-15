d66450262ce06cb02329070046106979b36382cf
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-15 14:08:50 +0700

    Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите через пробел два числа, где 1-е это производное, а 2-е степень");
+string[] array = Console.ReadLine().Split(" ");
+int a = int.Parse(array[0]);
+int b = int.Parse(array[1]);
+int c = 1;
+
+for (int i = 1; i <= b; i++)
+    c = c * a;
+Console.WriteLine($"Число {a} в степени {b} равно {c}");
